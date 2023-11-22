6b0c875356f0b788082e46a9f5ae1dd46da168f7
Carlos Hernandez <101907419+lZero070@users.noreply.github.com>
2023-11-21 20:57:07 -0500

    .

---
+++ Interprete/js/consola.js
@@ -0,0 +1,91 @@
+let jqconsole = $('#console').jqconsole('', '>>>');
+const iniciar = document.getElementById('runbtn');
+iniciar.addEventListener("click", resultado);
+const validarr = document.getElementById('valbtn');
+validarr.addEventListener("click", validar);
+const conso = document.getElementById('console');
+const tema = document.getElementById('clickDarkMode');
+const body = document.body;
+
+tema.addEventListener('change', function() {
+    if (this.checked) {
+        editor.setTheme("ace/theme/textmate");
+        body.classList.add('light-theme');
+        body.classList.remove('dark-theme');
+    } else {
+        editor.setTheme("ace/theme/idle_fingers");
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
+    }
+  });
+
+let startPrompt = function () {
+  jqconsole.Prompt(true, function (input) {
+    jqconsole.Write(input + '\n', 'jqconsole-output');
+    startPrompt();
+  });
+};
+
+
+jqconsole.Write('>> ','jqconsole-output');
+
+let editor = ace.edit("editor");
+editor.setTheme("ace/theme/idle_fingers");
+editor.getSession().setMode("ace/mode/python");
+
+
+
+function resultado(){
+    speechSynthesis.cancel();
+    conso.style.display = "block";
+    resetForRun();
+	try {
+		let output = gramatica.parse(editor.getValue()+'\n')
+		if(output == true) {
+			jqconsole.Write('Running program:\n', 'jqconsole-output');
+			eval(finalprogram);
+			jqconsole.Write('\n', 'jqconsole-output');
+		} 
+	} catch(exception) {
+		jqconsole.Write(exception + '\n\n', 'jqconsole-output');
+        voz(exception)
+	}
+	jqconsole.Write('>> ','jqconsole-output');
+}
+
+function validar(){
+    speechSynthesis.cancel();
+    conso.style.display = "block";
+    resetForRun();
+	try {
+		let output = gramatica.parse(editor.getValue()+'\n')
+		if(output == true) {
+			jqconsole.Write('The program contains no errors.', 'jqconsole-output');
+			jqconsole.Write('\n', 'jqconsole-output');
+            let texto = 'The program contains no errors.'
+            voz(texto);
+		} 
+	} catch(exception) {
+		jqconsole.Write(exception + '\n\n', 'jqconsole-output');
+        voz(exception)
+	}
+	jqconsole.Write('>> ','jqconsole-output');
+}
+
+$( "#clearbtn" ).click(function() {
+	jqconsole.Reset();
+});
+
+$( "#silbtn" ).click(function() {
+	speechSynthesis.cancel();;
+});
+
+function voz(texto){
+    let mensaje = new SpeechSynthesisUtterance(texto);
+    mensaje.pitch = -1;
+    setTimeout(() => {
+      speechSynthesis.speak(mensaje);
+    }, 100);
+}
+
+ 
